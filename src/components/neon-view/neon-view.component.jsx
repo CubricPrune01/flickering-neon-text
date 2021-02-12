47b4473d2ea50082443af6259f1703290cc48fd5
AsuraOZ <brian_lee0331@hotmail.com>
2021-02-12 02:37:56 -0500

    Flickering neon sign is initially completed and have responsive size css

---
+++ src/components/neon-view/neon-view.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import './neon-view.styles.css'
+
+const NeonView = () => {
+    return (
+        <div className='section'>
+            <div className='container'>
+                <h2>qa95swac595ssc58Irbdrty2oalsswzx12d</h2>
+                <h2>ww95swac595ssc58Irbdrty6oalsswzdq2d</h2>
+                <h2>duekadzp4ukalzp4Ve2xee4ee2xee41d22d</h2>
+                <h2>f95rswac595ssc58Irbdrtydoalsswzfw3d</h2>
+                <h2>rqwwhscws4d5wfrfshsw48d5aav3hjk3efd</h2>
+                <h2>tf<span className='text4'>b</span>2<span className='text2'>C</span>h<span className='text3'>l</span>sg46sacxcptrhxmsg46saaxc4fdd</h2>
+                <h2>gy<span className='text4'>r</span>e<span className='text2'>h</span>a<span className='text3'>e</span>sdvjs8sdvCddssdse<span className='text5'>express</span>3ggd</h2>
+                <h2>gu<span className='text4'>i</span>r<span className='text2'>e</span>d<span className='text3'>e</span>84asfge4aRsd5c21sds5c21swd2d</h2>
+                <h2>di<span className='text4'>a</span>t<span className='text2'>o</span>x8nvdde<span>Web</span>8gbs83s52fs83s52dggd</h2>
+                <h2>2i<span className='text4'>n</span>y<span className='text2'>n</span>s5ek25ed2<span>Developer</span>vamkdalvad3d</h2>
+                <h2>w33f<span className='text2'>g</span>aeodwposwshvk2vk3fAaldkxswrssd</h2>
+                <h2>95ff4swac595ssc58Irbdrty2oalsswzxd2</h2>
+                <h2>ukfgwadzp4ukalzp4Ve2xee4ee2xee41d1d</h2>
+                <h2>pdthwrhxmsptrhxmsE46sa<span className='text6'>postgres</span>cxfdxd</h2>
+                <h2>jajes<span className='text7'>React</span>dssdse3jafs8sdc2dssdseessd</h2>
+                <h2>kskrv5echekjc<span className='text8'>Node</span>kjuefv5a25cveyn3ddd</h2>
+                <h2>kslrv5echefkjc2s5vkjuev5a25cveynd1g</h2>
+                <h2>ukgfadzp4fukalzp4Ve2xee4ee2xee41dd2</h2>
+                <h2>95kfswac59f5ssc58Irbdrtyxoalsswzd2x</h2>
+                <h2>ukdgadzp4ukalzfp4Ve2xee4ee2xee41d36</h2>
+                <h2>ukdgadzp4ukalzpf4Ve2xee4ee2xee41dsb</h2>
+            </div>
+        </div>
+    )
+}
+
+export default NeonView;
